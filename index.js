913d81eb4bcfb1a0fe3f175f1fb50d5cdbf96577
Fortune Mohammed <fortunemoha14@gmail.com>
2025-03-16 12:52:12 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file!
+//set companyName to Scuber
+//let companyName = "Scober";
+//companyName = "Scuber";
+//let companyName = 'Scuber';
+//let companyName = 'Scuber';
+//const companyName = 'Scuber';
+//const companyName = 'Scuber';
+//while trying to redeclare an error occurs
+const companyName = 'Scuber';
+// mostProfitableNeighborhood is declared as equal to Chelsea:
+let mostProfitableNeighborhood = 'Chelsea'
+// companyCeo is declared as equal to Susan Smith:
+let companyCeo = 'Susan Smith'
+
